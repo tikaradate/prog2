67c253d34cc67578ff648f2f33bf9541658e07c1
tikaradate <vtvd19@urquell>
2020-03-09 17:14:26 -0300

    dicionario feito

--- proj1/proj1.c
+++ proj1/proj1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define PLCHLD 2
-#define MAX 100
+#define ALOC 10000
+#define MAXWRD 100
 
 int main(){
-	int i;
-	char **dicionario, atual[MAX];
+	int i, j, quant;
+	char **dicionario, atual[MAXWRD];
 	FILE *arq;
 
 	arq = fopen("brazilian", "r");
@@ -16,26 +16,28 @@ int main(){
 	}
 
 // alocacao inicial do dicionario
-	dicionario = malloc(PLCHLD * sizeof(char *));
+	quant = ALOC;
+	dicionario = malloc(quant * sizeof(char *));
 
-	for(i = 0; i < PLCHLD; i++){
-		dicionario[i] = malloc(MAX * sizeof(char));
-		printf("%p\n", dicionario[i]);
+	for(i = 0; i < quant; i++){
+		dicionario[i] = malloc(MAXWRD * sizeof(char));
 	}
 
 	i = 0;
-	fgets(atual, MAX, arq);
-	atual[strcspn(atual, "\n")] = 0;
-	strcpy(dicionario[i],atual);
-	printf("%p %s\n",dicionario[i], atual);
-	i++;
-	
-	fgets(atual, MAX, arq);
-	atual[strcspn(atual, "\n")] = 0;
-	strcpy(dicionario[i],atual);
-	printf("%p %s\n", dicionario[i], atual);
-
-	printf("%d %s %s\n", i, dicionario[0], dicionario[1]);
-
+	while(fgets(atual, MAXWRD, arq) != NULL){
+		atual[strcspn(atual, "\n")] = 0;
+		strcpy(dicionario[i],atual);
+		i++;
+		if(i == quant){
+			quant += ALOC;
+			dicionario = realloc(dicionario, quant * sizeof(char *));
+			for(j = i; j < quant; j++){
+				dicionario[j] = malloc(MAXWRD * sizeof(char));
+			}
+		}
+	}
 	fclose(arq);
+// fim alocacao dicionario
+
+
 }
