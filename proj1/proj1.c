76dd2d38291d13e2eb994d1ee30db6b6d2d03d7e
tikaradate <tikaradate@gmail.com>
2020-03-10 11:12:29 -0300

    busca binaria feita

--- proj1/proj1.c
+++ proj1/proj1.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define ALOC 10000
+#define ALOC 100000
 #define MAXWRD 100
 
+int busca_bin(char *chave, char **array, int elem){
+	int ini, meio, fim, teste;
+	
+	ini = 0;
+	meio = elem/2;
+	fim = elem;
+	while(ini != fim){
+		teste = strcmp(chave, array[meio]);
+		if(teste == 0){
+			return 1;
+		} else if(teste > 0) {
+			ini = meio+1;
+		} else {
+			fim = meio-1;
+		}
+		meio = (ini + fim)/2;
+	}
+	return 0;
+}
+
+
 int main(){
-	int i, j, quant;
-	char **dicionario, atual[MAXWRD];
+	int i, j, quant, achou;
+	char **dicionario, atual[MAXWRD], *palavra;
 	FILE *arq;
 
+// alocacao inicial do dicionario
 	arq = fopen("brazilian", "r");
 	if(!arq){
 		printf("Erro ao abrir brazilian\n");
 		exit(1);
 	}
 
-// alocacao inicial do dicionario
 	quant = ALOC;
 	dicionario = malloc(quant * sizeof(char *));
 
@@ -38,6 +59,11 @@ int main(){
 	}
 	fclose(arq);
 // fim alocacao dicionario
-
-
+	
+	printf("%d\n", i);
+	palavra = "arroz";
+	achou = busca_bin(palavra, dicionario, i);
+	if(achou){
+		printf("urra\n");
+	}
 }
