76d100423838f69f3367caafb1c83d145fe1aba1
tikaradate <tikaradate@gmail.com>
2020-03-11 23:35:08 -0300

    trabalho terminado, arrumar [a]

--- proj1/proj1.c
+++ proj1/proj1.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <locale.h>
 #include <ctype.h>
 #define ALOC 100000
 #define MAXWRD 100
 
 int busca_bin(char *chave, char **array, int elem){
+	if(strlen(chave) == 0)
+		return 1;
 	int ini, meio, fim, teste;
 	
 	ini = 0;
 	meio = elem/2;
 	fim = elem;
-	while(ini != fim){
-		teste = strcoll(chave, array[meio]);
+	while(ini <= fim){
+		teste = strcasecmp(chave, array[meio]);
 		if(teste == 0){
 			return 1;
 		} else if(teste > 0) {
@@ -26,36 +29,12 @@ int busca_bin(char *chave, char **array, int elem){
 	return 0;
 }
 
-int isLetter(unsigned char c){
-	if((c >= 'A' && c < 'Z')  || (c >= 'a' && c <= 'z') ||
-	   (c >= 192 && c <= 214) || (c >= 216 && c <= 246) ||
-	   (c >= 248 && c <= 255))
-		return 1;
-	return 0;
-}
-
-void removeSimbolos(char *string){
-	int i, j;
-	
-	i = 0;
-	j = 0;
-	while(string[i] != '\0'){
-		if(isalpha(string[i])){
-			string[j] = string[i];
-			j++;
-		}
-		i++;
-	}
-	string[j] = '\0';
-}
-
 int main(){
-	int i, j, quant, achou;
-	char **dicionario, atual[MAXWRD], c, *locale;
+	int i, j, quant, c;
+	char **dicionario, atual[MAXWRD];
 	FILE *arq;
 
-	locale = setlocale (LC_CTYPE, "pt_BR.ISO-8859-1") ;
-	printf("locale %s\n", locale);
+	setlocale (LC_CTYPE, "pt_BR.ISO-8859-1") ;
 // alocacao inicial do dicionario
 	arq = fopen("brazilian", "r");
 	if(!arq){
@@ -73,7 +52,7 @@ int main(){
 	i = 0;
 	while(fgets(atual, MAXWRD, arq)){
 		atual[strcspn(atual, "\n")] = 0;
-		strcpy(dicionario[i],atual);
+		strcpy(dicionario[i], atual);
 		i++;
 		if(i == quant){
 			quant += ALOC;
@@ -86,8 +65,20 @@ int main(){
 	fclose(arq);
 // fim alocacao dicionario
 
-	while((scanf("%s", atual)) != EOF){
-		removeSimbolos(atual);
-		printf("%s ", atual);
+	j = 0;
+	while((c = getchar())!= EOF){
+		if(isalpha(c)){
+			atual[j] = c;
+			j++;
+		} else {
+			atual[j] = '\0';
+			if(!busca_bin(atual, dicionario, i)){
+				printf("[%s]", atual);
+			} else {
+				printf("%s", atual);
+			}
+			printf("%c", c);
+			j = 0;
+		}
 	}
 }
--- proj1/soc.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	char s[100];
-	while(scanf("%s", s)){
-	printf("%s", s);
-	}
-}
